891d56330d732803f534cdf8bc93516e3fc5ea4c
dloslier <dimitri.loslier@euroedi.com>
2025-09-04 14:10:16 +0200

    commit

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
-console.log("test");
+// console.log("test");
 
 fetch('page_header.html')
       .then(response => response.text())
       .then(html => {
-        document.getElementById('header').innerHTML = html;
+        document.getElementById('id_header').innerHTML = html;
       });
-    fetch('index_2.html')
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('content').innerHTML = html;
-      });
+//     fetch('index_2.html')
+//       .then(response => response.text())
+//       .then(html => {
+//         document.getElementById('content').innerHTML = html;
+//       });
     fetch('page_footer.html')
       .then(response => response.text())
       .then(html => {
-        document.getElementById('footer').innerHTML = html;
+        document.getElementById('id_footer').innerHTML = html;
       });
 
-function page_2() {
-    fetch('index_2.html')
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('content').innerHTML = html;
-    });
-}
+// function page_2() {
+//     fetch('index_2.html')
+//     .then(response => response.text())
+//     .then(html => {
+//       document.getElementById('content').innerHTML = html;
+//     });
+// }
 
-function page_3() {
-    fetch('index_3.html')
+// function page_3() {
+//     fetch('index_3.html')
+//     .then(response => response.text())
+//     .then(html => {
+//       document.getElementById('content').innerHTML = html;
+//     });
+// }
+
+
+
+// function set_page_home() {
+
+// }
+// function set_page_gallery() {
+
+// }
+// function set_page_contact() {
+
+// }
+
+function set_page(string_page_) {
+    var string_page = string_page_ + ".html"
+    fetch(string_page)
     .then(response => response.text())
     .then(html => {
-      document.getElementById('content').innerHTML = html;
+      document.getElementById('id_content').innerHTML = html;
     });
 }

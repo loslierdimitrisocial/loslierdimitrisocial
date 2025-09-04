a724a2aa9449d671330faf5159b1356d1d07c70c
dloslier <dimitri.loslier@euroedi.com>
2025-09-04 14:16:41 +0200

    commit

--- script.js
+++ script.js
@@ -1,54 +1,17 @@
-// console.log("test");
-
-fetch('page_header.html')
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('id_header').innerHTML = html;
-      });
-//     fetch('index_2.html')
-//       .then(response => response.text())
-//       .then(html => {
-//         document.getElementById('content').innerHTML = html;
-//       });
+function loadPage(page) {
+    fetch('page_header.html')
+      .then(res => res.text())
+      .then(data => document.getElementById('header').innerHTML = data);
+  
+    fetch(page)
+      .then(res => res.text())
+      .then(data => document.getElementById('content').innerHTML = data);
+  
     fetch('page_footer.html')
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('id_footer').innerHTML = html;
-      });
-
-// function page_2() {
-//     fetch('index_2.html')
-//     .then(response => response.text())
-//     .then(html => {
-//       document.getElementById('content').innerHTML = html;
-//     });
-// }
-
-// function page_3() {
-//     fetch('index_3.html')
-//     .then(response => response.text())
-//     .then(html => {
-//       document.getElementById('content').innerHTML = html;
-//     });
-// }
-
-
-
-// function set_page_home() {
-
-// }
-// function set_page_gallery() {
-
-// }
-// function set_page_contact() {
-
-// }
-
-function set_page(string_page_) {
-    var string_page = string_page_ + ".html"
-    fetch(string_page)
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById('id_content').innerHTML = html;
-    });
-}
+      .then(res => res.text())
+      .then(data => document.getElementById('footer').innerHTML = data);
+  }
+  
+  // Load default page
+  window.onload = () => loadPage('page_home.html');
+  

933c6ec2429dfba3673863e405f176a7bcb44248
dloslier <dimitri.loslier@euroedi.com>
2025-09-09 14:56:19 +0200

    commit

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-function loadPage(page) {
-    fetch('page_header.html')
+function set_page(string_page_, string_id_) {
+    fetch('page_' + string_page_ + '.html')
       .then(res => res.text())
-      .then(data => document.getElementById('header').innerHTML = data);
-  
-    fetch(page)
-      .then(res => res.text())
-      .then(data => document.getElementById('content').innerHTML = data);
-  
-    fetch('page_footer.html')
-      .then(res => res.text())
-      .then(data => document.getElementById('footer').innerHTML = data);
-  }
-  
-  // Load default page
-  window.onload = () => loadPage('page_home.html');
-  
+      .then(data => document.getElementById('id_' + string_id_).innerHTML = data);
+}
+
+set_page("home", "content")
+set_page("header", "header")
+set_page("footer", "footer")

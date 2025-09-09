2d2dba8ff96b665c499252196d5395c77dc0c04b
gamebuilderfr@gmail.com <KZ9Rk7K5pphFKhQryFBQMpTFHgCzXTjG>
2025-09-09 20:37:05 +0100

    commit
    no image in repo
    - image in main machine only
    need editing, censor and compression

--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 function set_page(string_page_, string_id_) {
     fetch('page_' + string_page_ + '.html')
-      .then(res => res.text())
-      .then(data => document.getElementById('id_' + string_id_).innerHTML = data);
+        .then(res => res.text())
+        .then(data => document.getElementById('id_' + string_id_).innerHTML = data);
 }
 
 set_page("home", "content")

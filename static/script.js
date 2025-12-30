e4437069f4a1d079ad5e964184d27023d3c1855c
dloslier <Dimitri.Loslier@euroedi.com>
2025-12-30 11:54:21 +0100

    commit

--- static/script.js
+++ static/script.js
@@ -5,6 +5,28 @@ async function fetch_page(string_page_, string_id_) {
     div.innerHTML = content;
 }
 
+async function button_clipboard(string_id_) {
+    var text = document.getElementById(string_id_).innerHTML;
+    await navigator.clipboard.writeText(text);
+
+    showAlert("test")
+}
+
+function showAlert(message) {
+    const alertBox = document.getElementById("alert");
+    alertBox.textContent = message;
+
+    alertBox.style.display = "block";
+    requestAnimationFrame(() => {
+        alertBox.style.opacity = 1;
+    });
+
+    setTimeout(() => {
+        alertBox.style.opacity = 0;
+        setTimeout(() => alertBox.style.display = "none", 500);
+    }, 2000);
+}
+
 fetch_page("page_header.html", "header");
 fetch_page("page_footer.html", "footer");
 fetch_page("page_home.html", "content");
